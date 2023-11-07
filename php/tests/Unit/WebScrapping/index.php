27fca174a4a2112e7ef50b3080959565d2dd4267
Amanda Letícia <102308691+amandaalvees@users.noreply.github.com>
2023-11-07 02:48:10 -0300

    Finalização do teste
    Concluí

--- php/tests/Unit/WebScrapping/index.php
+++ php/tests/Unit/WebScrapping/index.php
@@ -1,13 +1,30 @@
 <?php
 
-$html = file_get_contents('webscrapping/origin.html');
+require 'vendor/autoload.php';
+libxml_use_internal_errors(true);
 
-$domDocument = new DOMDocument();
-$domDocument->loadHTML($html);
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
+$conteudo = file_get_contents('https://proceedings.science/nmrmeeting/18nmrmeeting-2021/trabalhos?check_logged_in=1');
+$documento = new DOMDocument();
+$documento->loadHTML($conteudo);
 
+$xPath = new DOMXPath($documento);
+$domNodeList = $xPath->query('//*[@id="hits-container"]/div/div[1]/a/h4');
 
+$spreadsheet = new Spreadsheet();
+$sheet = $spreadsheet->getActiveSheet();
+$sheet->setCellValue('A1', 'Título');
 
+$row = 2;
+foreach ($domNodeList as $elemento) {
+    $sheet->setCellValue('A' . $row, $elemento->textContent);
+    $row++;
+}
 
+$writer = new Xlsx($spreadsheet);
+$writer->save('planilha.xlsx');
+echo 'Planilha Excel criada com sucesso!';
 
 ?>
